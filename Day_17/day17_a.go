7888762402135f2864e8b00bfd1a4663af72064f
lynerist <leonardo.albani@studenti.unimi.it>
2020-12-17 13:04:45 +0100

    add day 17 half

---
+++ Day_17/day17_a.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+type cube struct{
+ 	active		bool
+	adjacents	int
+}
+
+func main() {
+	//	--- STANDARD FILE OPENING ---
+	input, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err, "\nYou need a file called input.txt in this directory")
+		return
+	}
+	defer input.Close()
+
+	// --- START ---
+	sc := bufio.NewScanner(input)
+
+	poketDimension := make(map[string]cube)
+	
+	var countStartY int
+	for sc.Scan(){
+		for x, active := range sc.Text(){
+			key := fmt.Sprintf("%d,%d,0",x,countStartY)
+			if active == '#'{
+				poketDimension[key] = cube{true, 0}
+			}
+		}
+		countStartY++
+	}
+
+	for cycle :=0; cycle<6; cycle++{
+		countNeighbours(poketDimension)
+		changeState(poketDimension)
+	}
+	fmt.Println(len(poketDimension))
+}
+
+func countNeighbours(poketDimension map[string]cube){
+	for coordinates, cube := range poketDimension{
+		if !cube.active{
+			continue
+		}
+		var x,y,z int
+		fmt.Sscanf(coordinates,"%d,%d,%d",&x,&y,&z)
+		
+		for i:=-1; i<=1; i++{
+			for j:=-1; j<=1; j++{
+				for k:=-1; k<=1; k++{
+					adjacent := fmt.Sprintf("%d,%d,%d",x+i,y+j,z+k)
+					adjacentCube := poketDimension[adjacent]
+					if adjacent != coordinates {
+						adjacentCube.adjacents++
+						poketDimension[adjacent] = adjacentCube
+					}
+				}
+			}
+		}
+	}
+}
+
+func changeState(poketDimension map[string]cube){
+	for coordinates, currentCube := range poketDimension{
+		if currentCube.active && (currentCube.adjacents < 2 || currentCube.adjacents > 3) || !currentCube.active && currentCube.adjacents != 3{
+			delete(poketDimension, coordinates)
+		}else{
+			poketDimension[coordinates] = cube{true, 0}
+		}
+	}
+}
